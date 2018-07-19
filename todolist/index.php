375656f35bfc30d07d2da112c08c2ba725377585
ericLavachery <eric.lavachery@gmail.com>
2018-07-19 14:07:02 +0200

    ok pour enregistrement des tâches!

--- todolist/index.php
+++ todolist/index.php
@@ -1,36 +1,31 @@
-
-
 <?php
-if (isset($_POST['task'])) {
-
-    if(file_exists('todo.json'))
-    {
-        // $oldData = array();
+if(file_exists('todo.json'))
+{
+    if (isset($_POST['task'])) {
         $oldDataJSON = file_get_contents('todo.json');
         $oldData = json_decode($oldDataJSON, true);
-        var_dump($oldData);
-        echo "<br><br>";
-        $extraData = array(
-            'task' => $_POST['task'],
-            'done' => $_POST["done"],
-            'edit' => $_POST["edit"]
-        );
-        var_dump($extraData);
-        echo "<br><br>";
-        // $oldData[] = $extraData;
+        // var_dump($oldData);
+        // echo "<br><br>";
+        $firstTask['task'] = $_POST['task'];
+        $firstTask['done'] = $_POST["done"];
+        $extraData[1] = $firstTask;
+        // var_dump($extraData);
+        // echo "<br><br>";
         $updatedData = array_merge($oldData,$extraData);
-        var_dump($updatedData);
-        echo "<br><br>";
+        // var_dump($updatedData);
+        // echo "<br><br>";
         $updatedDataJSON = json_encode($updatedData);
-        // if(file_put_contents('todo.json', $updatedDataJSON)){
-        //     $addMessage = "<p>Tâche rajoutée!</p>";
-        // }
+        if(file_put_contents('todo.json', $updatedDataJSON)){
+            $addMessage = "<p>Tâche rajoutée!</p>";
+        }
     }
-    else
-    {
-        file_put_contents('todo.json', json_encode($_REQUEST));
-    }
-
+}
+else
+{
+    $firstTask['task'] = 'Aquaponey';
+    $firstTask['done'] = 'yes';
+    $baseTask[0] = $firstTask;
+    file_put_contents('todo.json', json_encode($baseTask));
 }
 ?>
 
@@ -49,9 +44,6 @@ if (isset($_POST['task'])) {
 
     <?php include 'content.php' ?>
     <?php include 'form.php' ?>
-
-    <br><br>
-    <?php echo json_encode($_REQUEST) ?>
 
 </body>
 </html>
